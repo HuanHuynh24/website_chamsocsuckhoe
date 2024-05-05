a6910975eb13c3ce7b7498f8659ff55684b42a75
hieudiepvan <sdt0354868443@gmail.com>
2024-05-05 22:53:45 +0700

    new

--- src/component/page/js/login.js
+++ src/component/page/js/login.js
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGooglePlusG, faFacebookF } from "@fortawesome/free-brands-svg-icons";
+import React, { useState, useEffect } from "react";
 import "../css/login.css";
 
 const Login = () => {
@@ -20,72 +18,71 @@ const Login = () => {
         console.log("Password:", password);
     };
 
+    useEffect(() => {
+        const signUpButton = document.getElementById('signUp');
+        const signInButton = document.getElementById('signIn');
+        const container = document.querySelector('.container-login'); 
+
+        signUpButton.addEventListener('click', () => {
+            container.classList.add("right-panel-active");
+        })
+
+        signInButton.addEventListener('click', () => {
+            container.classList.remove("right-panel-active");
+        })
+
+        return () => {
+            signUpButton.removeEventListener('click', () => {
+                container.classList.add("right-panel-active");
+            });
+
+            signInButton.removeEventListener('click', () => {
+                container.classList.remove("right-panel-active");
+            });
+        };
+    }, []);
+
     return (
-        <div className="login-background">
-            <div className="login-container">
-                <div className="login-content">
+        <div className="login">
+            <div className="container-login">
+            <div className="form-container sign-up-container">
+                <form action="#">
+                    <h1>Create Account</h1>
+                    <span>or use your email for registration</span>
+                    <input type="text" placeholder="UserName" value={username} onChange={handleUsernameChange}/>
+                    <input type="email" placeholder="Email"/>
+                    <input type="tel" placeholder="Phone Number" />
+                    <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange}/>  
+                    <button onClick={handleLogin}>Sign Up</button>
+                </form>
+            </div>
+            <div className="form-container sign-in-container">
+                <form action="#">
+                    <h1>Sign In</h1>
+                    <span>or use your account</span>
+                    <input type="text" placeholder="UserName" value={username} onChange={handleUsernameChange}/>
+                    <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange}/> 
+                    <button onClick={handleLogin}>Sign In</button>
+                </form>
+            </div>
+            <div className="overlay-container">
+                <div className="overlay-login">
                     
-                    <div className="row">
-                        <div className="col-12 text-login">Login</div>
+                    <div className="overlay-panel overlay-left">
+                        <h1>Welcome To FF!</h1>
+                        <p>Please Subscribe and Support to our channel</p>
+                        <button className="press" id="signIn">Sign In</button>
                     </div>
 
-                    <div className="row">
-                        <div className="col-12 form-group login-input">
-                            <div><label htmlFor="username">Username</label></div>                              
-                            <input
-                                type="text"
-                                id="username"
-                                className="form-control"
-                                placeholder="Enter your username"
-                                value={username}
-                                onChange={handleUsernameChange}
-                            />
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="col-12 form-group login-input">
-                            <div><label htmlFor="password">Password</label></div>                             
-                            <input
-                                type="password"
-                                id="password"
-                                className="form-control"
-                                placeholder="Enter your password"
-                                value={password}
-                                onChange={handlePasswordChange}
-                            />
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="col-12 login-input">
-                            <button className="btn btn-login" onClick={handleLogin}>
-                                Login
-                            </button>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="col-12">
-                            <span className="forgot-password">Forgot your Password</span>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="col-12 text-center mt-3">
-                            <span className="text-other-login">Or login with:</span>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="social-login">
-                            <FontAwesomeIcon icon={faGooglePlusG} className="google" />
-                            <FontAwesomeIcon icon={faFacebookF} className="facebook" />
-                        </div>
+                    <div className="overlay-panel overlay-right">
+                        <h1>Frontend Forever</h1>
+                        <p>Learn web designing from here in tamil and english</p>
+                        <button className="press" id="signUp">Sign In</button>
                     </div>
                 </div>
             </div>
-        </div>
+            </div>
+        </div>        
     );
 };
 
